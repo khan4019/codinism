7ebe970592a93e9d8f60d9deb0d1919fc61cd612
Your Name <AhmmedRasel2@Gmail.com>
2018-10-20 23:28:07 +0600

    email sender added

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -13,5 +13,4 @@
     
     $body = "Here is the message you got:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
     $send = mail($to, $subject, $body, $headers);
-
 ?>
