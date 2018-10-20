98142784b430d30317ce0c4c79df8a6288ada316
Your Name <AhmmedRasel2@Gmail.com>
2018-10-20 23:23:50 +0600

    email send notification

--- js/validator.js
+++ js/validator.js
@@ -3,7 +3,6 @@ function check_Email(mail) {
     var regex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
     if (regex.test(mail.email.value)) {
         return true;
-        alert("Congrats! Your email has been send send...");
         $('.hover-tooltip').tooltip('disable')
     }else {
         $('[data-toggle="tooltip"], .tooltip').tooltip("show");
@@ -20,7 +19,26 @@ function check_Email(mail) {
         $('.hover-tooltip').tooltip('hide')
     })
     $("#submit").click(function() {
-        $(".hover-tooltip").tooltip("enable");
+      $("#submit")[0].reset();
     })
   
   });
+  
+  $('#form').on('submit', function(event) {
+ event.preventDefault(); //stops form on submit
+  var formData = {};
+  $.each($("#form").serializeArray(), function (i, field) {
+    formData[field.name] = field.value;
+  });
+  $.ajax({
+  url: 'inc/sendemail.php',
+  data: formData,
+  method:'POST',
+  success: function(response) { 
+   $(this).hide(); 
+   $('#Mymodal').modal('show');
+   document.getElementById('form').reset();
+   }
+});    
+
+});
